0dcea0f61859543b209e099816eb068e61ea44c6
sdldp <2403379772@qq.com>
2019-07-16 14:13:26 +0800

    mf app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use( async (ctx, err) => {
+app.use( async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>hello koa</h1>'
+    ctx.response.body = '<h1>hello koa!</h1>'
 
 });
 
-app.listen(300);
+app.listen(3000);
 console.log('http://127.0.0.1:3000');
